f57100ea64d526ef58d591e658091b9d3af22153
Aparajita <aparajita.243432@gmail.com>
2024-09-24 13:26:41 +0530

    validattion

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,42 +1,42 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
 
 const addTask = () => {
-  const taskValue = inputBox.value.trim();
-  if (taskValue === "") {
-    alert("You must write something!");
-  } else {
-    let li = document.createElement("li");
-    li.innerHTML = taskValue;
-    listContainer.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7"; // Cross icon (X)
-    li.appendChild(span);
-  }
-  inputBox.value = "";
-  saveData();
+    const taskValue = inputBox.value.trim();
+    if (taskValue === '') {
+        alert('You must write something!');
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = taskValue;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7'; // Cross icon (X)
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
 };
 
 listContainer.addEventListener(
-  "click",
-  function (e) {
-    if (e.target.tagName === "LI") {
-      e.target.classList.toggle("checked");
-      saveData();
-    } else if (e.target.tagName === "SPAN") {
-      e.target.parentElement.remove();
-      saveData();
-    }
-  },
-  false
+    'click',
+    (e) => {
+        if (e.target.tagName === 'LI') {
+            e.target.classList.toggle('checked');
+            saveData();
+        } else if (e.target.tagName === 'SPAN') {
+            e.target.parentElement.remove();
+            saveData();
+        }
+    },
+    false
 );
 
 const saveData = () => {
-  localStorage.setItem("data", listContainer.innerHTML); // Save list data to localStorage
+    localStorage.setItem('data', listContainer.innerHTML); // Save list data to localStorage
 };
 
 const showTask = () => {
-  listContainer.innerHTML = localStorage.getItem("data") || ""; // Display saved tasks if available
+    listContainer.innerHTML = localStorage.getItem('data') || ''; // Display saved tasks if available
 };
 
 showTask();
